adff0f5b7cb6d93700882e738c4335479b6e0b22
perillaroc <perillaroc@gmail.com>
2020-02-09 19:04:55 +0800

    :recycle: extract create consumers into new functions.

--- cmd/nwpc_message_consumer/app/production.go
+++ cmd/nwpc_message_consumer/app/production.go
@@ -35,66 +35,66 @@ Consume production message from rabbitmq and store them into elasticsearch.
 
 var productionCmd = &cobra.Command{
 	Use:   "production",
-	Short: "consume message of production",
+	Short: "consume production message",
 	Long:  productionLongDescription,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		var currentConsumer consumer.Consumer = nil
+		currentSource := consumer.RabbitMQSource{
+			Server:   rabbitmqServer,
+			Exchange: "nwpc.operation.production",
+			Topics:   []string{"*.production.*"},
+			Queue:    rabbitmqQueueName,
+		}
+
 		if consumerType == "print" {
-			log.WithFields(log.Fields{
-				"component": "production",
-				"event":     "consumer",
-			}).Info("start to consume...")
-
-			consumer := &consumer.PrinterConsumer{
-				Source: consumer.RabbitMQSource{
-					Server:   rabbitmqServer,
-					Exchange: "nwpc.operation.production",
-					Topics:   []string{"*.production.*"},
-					Queue:    rabbitmqQueueName,
-				},
-				WorkerCount: workerCount,
-				Debug:       isDebug,
-			}
-
-			err := consumer.ConsumeMessages()
-			if err != nil {
-				log.WithFields(log.Fields{
-					"component": "production",
-					"event":     "consumer",
-				}).Errorf("%v", err)
-			}
-			return err
+			currentConsumer = createPrinterConsumer(currentSource, workerCount, isDebug)
 		} else if consumerType == "elasticsearch" {
-			log.WithFields(log.Fields{
-				"component": "ecflow-client",
-				"event":     "consumer",
-			}).Info("start to consume...")
-
-			consumer := &consumer.ProductionConsumer{
-				Source: consumer.RabbitMQSource{
-					Server:   rabbitmqServer,
-					Exchange: "nwpc.operation.production",
-					Topics:   []string{"*.production.*"},
-					Queue:    rabbitmqQueueName,
-				},
-				Target: consumer.ElasticSearchTarget{
-					Server: elasticServer,
-				},
-				WorkerCount: workerCount,
-				BulkSize:    bulkSize,
-				Debug:       isDebug,
+			target := consumer.ElasticSearchTarget{
+				Server: elasticServer,
 			}
+			currentConsumer = createElasticSearchConsumer(currentSource, target, workerCount, isDebug)
+		}
 
-			err := consumer.ConsumeMessages()
-			if err != nil {
-				log.WithFields(log.Fields{
-					"component": "ecflow-client",
-					"event":     "consumer",
-				}).Errorf("%v", err)
-			}
-			return err
-		} else {
+		if currentConsumer == nil {
 			log.Fatal("consumer type is not supported: %s", consumerType)
 			return nil
 		}
+
+		log.WithFields(log.Fields{
+			"component": "production",
+			"event":     "consumer",
+		}).Info("start to consume...")
+
+		err := currentConsumer.ConsumeMessages()
+		if err != nil {
+			log.WithFields(log.Fields{
+				"component": "production",
+				"event":     "consumer",
+			}).Errorf("%v", err)
+		}
+		return err
 	},
+}
+
+func createPrinterConsumer(source consumer.RabbitMQSource, workerCount int, debug bool) *consumer.PrinterConsumer {
+	printerConsumer := &consumer.PrinterConsumer{
+		Source:      source,
+		WorkerCount: workerCount,
+		Debug:       debug,
+	}
+	return printerConsumer
+}
+
+func createElasticSearchConsumer(
+	source consumer.RabbitMQSource,
+	target consumer.ElasticSearchTarget,
+	workerCount int, debug bool) *consumer.ProductionConsumer {
+	elasticSearchConsumer := &consumer.ProductionConsumer{
+		Source:      source,
+		Target:      target,
+		WorkerCount: workerCount,
+		BulkSize:    bulkSize,
+		Debug:       debug,
+	}
+	return elasticSearchConsumer
 }
