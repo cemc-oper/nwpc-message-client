a639c1e200e50126655d7e70a3c1f8207d2adf5b
perillaroc <perillaroc@gmail.com>
2020-02-13 11:17:22 +0800

    :recycle: add BrokerSender.

--- cmd/nwpc_message_client/app/sender.go
+++ cmd/nwpc_message_client/app/sender.go
@@ -1,12 +1,9 @@
 package app
 
 import (
-	"context"
 	"fmt"
-	pb "github.com/nwpc-oper/nwpc-message-client/common/messagebroker"
 	"github.com/nwpc-oper/nwpc-message-client/common/sender"
 	log "github.com/sirupsen/logrus"
-	"google.golang.org/grpc"
 	"time"
 )
 
@@ -27,51 +24,24 @@ func sendMessage(server string, exchange string, routeKey string, messageBytes [
 }
 
 func sendMessageToBroker(server string, exchange string, routeKey string, messageBytes []byte) error {
-	var opts []grpc.DialOption
-	opts = append(opts, grpc.WithInsecure())
-	conn, err := grpc.Dial(brokerAddress, opts...)
+	brokerSender := sender.BrokerSender{
+		BrokerAddress:  brokerAddress,
+		RabbitMQServer: server,
+		Exchange:       exchange,
+		RouteKey:       routeKey,
+		WriteTimeout:   time.Second * 2,
+	}
+
+	err := brokerSender.SendMessage(messageBytes)
+
 	if err != nil {
 		log.WithFields(log.Fields{
 			"component": "sender",
 			"event":     "connection",
-		}).Errorf("connect to broker has error: %v\n", err)
-		return fmt.Errorf("connect to broker has error: %v\n", err)
+		}).Errorf("send message has error: %v\n", err)
 	}
 
-	defer conn.Close()
-
-	client := pb.NewMessageBrokerClient(conn)
-
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
-	defer cancel()
-
-	response, err := client.SendRabbitMQMessage(ctx, &pb.RabbitMQMessage{
-		Target: &pb.RabbitMQTarget{
-			Server:   server,
-			Exchange: exchange,
-			RouteKey: routeKey,
-		},
-		Message: &pb.Message{
-			Data: messageBytes,
-		},
-	})
-
-	if err != nil {
-		log.WithFields(log.Fields{
-			"component": "sender",
-			"event":     "send",
-		}).Errorf("send message has error: %v", err)
-		return fmt.Errorf("send message has error: %v", err)
-	}
-
-	if response.ErrorNo != 0 {
-		log.WithFields(log.Fields{
-			"component": "sender",
-			"event":     "response",
-		}).Errorf("send message return error code %d: %s", response.ErrorNo, response.ErrorMessage)
-		return fmt.Errorf("send message return error code %d: %s", response.ErrorNo, response.ErrorMessage)
-	}
-	return nil
+	return err
 }
 
 func sendMessageToRabbitmq(server string, exchange string, routeKey string, messageBytes []byte) error {
---
+++ common/sender/borker.go
@@ -0,0 +1,54 @@
+package sender
+
+import (
+	"context"
+	"fmt"
+	pb "github.com/nwpc-oper/nwpc-message-client/common/messagebroker"
+	"google.golang.org/grpc"
+	"time"
+)
+
+type BrokerSender struct {
+	BrokerAddress  string
+	RabbitMQServer string
+	Exchange       string
+	RouteKey       string
+	WriteTimeout   time.Duration
+}
+
+func (s *BrokerSender) SendMessage(message []byte) error {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+	conn, err := grpc.Dial(s.BrokerAddress, opts...)
+	if err != nil {
+		return fmt.Errorf("connect to broker has error: %v\n", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.NewMessageBrokerClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+
+	response, err := client.SendRabbitMQMessage(ctx, &pb.RabbitMQMessage{
+		Target: &pb.RabbitMQTarget{
+			Server:   s.RabbitMQServer,
+			Exchange: s.Exchange,
+			RouteKey: s.RouteKey,
+		},
+		Message: &pb.Message{
+			Data: message,
+		},
+	})
+
+	if err != nil {
+		return fmt.Errorf("send message has error: %v", err)
+	}
+
+	if response.ErrorNo != 0 {
+		return fmt.Errorf("send message return error code %d: %s", response.ErrorNo, response.ErrorMessage)
+	}
+
+	return nil
+}
