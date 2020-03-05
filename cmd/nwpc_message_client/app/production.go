42fae4f076e4fe33d807960b73b9043509b774f2
perillaroc <perillaroc@gmail.com>
2020-03-05 22:45:22 +0800

    :art: extract main flag set into a function.

--- cmd/nwpc_message_client/app/production.go
+++ cmd/nwpc_message_client/app/production.go
@@ -12,7 +12,10 @@ import (
 	"time"
 )
 
+const RequiredOption = "REQUIRED_OPTION"
+
 const ProductionMessageType = "production"
+
 const productionDescription = `
 Send messages for production.
 Messages are send to a rabbitmq server directly or via a broker running by broker command.
@@ -38,57 +41,9 @@ type productionCommand struct {
 }
 
 func (pc *productionCommand) runCommand(cmd *cobra.Command, args []string) error {
-	flagSet := pflag.NewFlagSet("oper", pflag.ContinueOnError)
-	flagSet.ParseErrorsWhitelist.UnknownFlags = true
-	flagSet.StringVar(&pc.system, "system", "",
-		"system name, such as grapes_gfs_gmf")
-	flagSet.StringVar(&pc.productionType, "production-type", "",
-		fmt.Sprintf("production type, such as %s", common.ProductionTypeGrib2))
-	flagSet.StringVar(&pc.stream, "production-stream", "",
-		"production stream, such as oper")
-
-	flagSet.StringVar(&pc.event, "event", "",
-		fmt.Sprintf("production event, such as %s", common.ProductionEventStorage))
-	flagSet.StringVar(&pc.status, "status", string(common.Complete),
-		fmt.Sprintf("event status, such as %s, %s", common.Complete, common.Aborted))
-
-	flagSet.StringVar(&pc.rabbitmqServer, "rabbitmq-server", "",
-		"rabbitmq server, such as amqp://guest:guest@host:port")
-
-	flagSet.BoolVar(&pc.useBroker, "with-broker", true,
-		"deliver message using a broker, should set --broker-address when enabled.")
-	flagSet.StringVar(&pc.brokerAddress, "broker-address", "",
-		"broker address, work with --with-broker")
-
-	flagSet.BoolVar(&pc.disableSend, "disable-send", false,
-		"disable message deliver, just for debug.")
-
-	flagSet.SortFlags = false
-	flagSet.ParseErrorsWhitelist = pflag.ParseErrorsWhitelist{UnknownFlags: true}
-
-	flagSet.SetAnnotation("system", RequiredOption, []string{"true"})
-	flagSet.SetAnnotation("production-type", RequiredOption, []string{"true"})
-	flagSet.SetAnnotation("production-stream", RequiredOption, []string{"true"})
-	flagSet.SetAnnotation("event", RequiredOption, []string{"true"})
-	flagSet.SetAnnotation("rabbitmq-server", RequiredOption, []string{"true"})
-
-	err := flagSet.Parse(args)
+	err := pc.parseMainOptions(args)
 	if err != nil {
-		return fmt.Errorf("parse options has error: %s", err)
-	}
-
-	var missingFlagNames []string
-	flagSet.VisitAll(func(flag *pflag.Flag) {
-		requiredAnnotation, found := flag.Annotations[RequiredOption]
-		if !found {
-			return
-		}
-		if (requiredAnnotation[0] == "true") && !flag.Changed {
-			missingFlagNames = append(missingFlagNames, flag.Name)
-		}
-	})
-	if len(missingFlagNames) > 0 {
-		return fmt.Errorf(`required flag(s) "%s" not set`, strings.Join(missingFlagNames, `", "`))
+		return fmt.Errorf("%v", err)
 	}
 
 	var data interface{}
@@ -170,7 +125,56 @@ func newProductionCommand() *productionCommand {
 	return pc
 }
 
-const RequiredOption = "REQUIRED_OPTION"
+func (pc *productionCommand) parseMainOptions(args []string) error {
+	flagSet := pc.generateCommandMainParser()
+	err := flagSet.Parse(args)
+	if err != nil {
+		return fmt.Errorf("parse options has error: %s", err)
+	}
+
+	err = checkRequiredFlags(flagSet)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	return nil
+}
+
+func (pc *productionCommand) generateCommandMainParser() *pflag.FlagSet {
+	flagSet := pflag.NewFlagSet("oper", pflag.ContinueOnError)
+	flagSet.ParseErrorsWhitelist.UnknownFlags = true
+	flagSet.StringVar(&pc.system, "system", "",
+		"system name, such as grapes_gfs_gmf")
+	flagSet.StringVar(&pc.productionType, "production-type", "",
+		fmt.Sprintf("production type, such as %s", common.ProductionTypeGrib2))
+	flagSet.StringVar(&pc.stream, "production-stream", "",
+		"production stream, such as oper")
+
+	flagSet.StringVar(&pc.event, "event", "",
+		fmt.Sprintf("production event, such as %s", common.ProductionEventStorage))
+	flagSet.StringVar(&pc.status, "status", string(common.Complete),
+		fmt.Sprintf("event status, such as %s, %s", common.Complete, common.Aborted))
+
+	flagSet.StringVar(&pc.rabbitmqServer, "rabbitmq-server", "",
+		"rabbitmq server, such as amqp://guest:guest@host:port")
+
+	flagSet.BoolVar(&pc.useBroker, "with-broker", true,
+		"deliver message using a broker, should set --broker-address when enabled.")
+	flagSet.StringVar(&pc.brokerAddress, "broker-address", "",
+		"broker address, work with --with-broker")
+
+	flagSet.BoolVar(&pc.disableSend, "disable-send", false,
+		"disable message deliver, just for debug.")
+
+	flagSet.SortFlags = false
+	flagSet.ParseErrorsWhitelist = pflag.ParseErrorsWhitelist{UnknownFlags: true}
+
+	flagSet.SetAnnotation("system", RequiredOption, []string{"true"})
+	flagSet.SetAnnotation("production-type", RequiredOption, []string{"true"})
+	flagSet.SetAnnotation("production-stream", RequiredOption, []string{"true"})
+	flagSet.SetAnnotation("event", RequiredOption, []string{"true"})
+	flagSet.SetAnnotation("rabbitmq-server", RequiredOption, []string{"true"})
+	return flagSet
+}
 
 func (pc *productionCommand) getOperationData(
 	cmd *cobra.Command, args []string,
@@ -193,19 +197,9 @@ func (pc *productionCommand) getOperationData(
 		return common.OperationProductionData{}, fmt.Errorf("parse options has error: %s", err)
 	}
 
-	var missingFlagNames []string
-	operFlagSet.VisitAll(func(flag *pflag.Flag) {
-		requiredAnnotation, found := flag.Annotations[RequiredOption]
-		if !found {
-			return
-		}
-		if (requiredAnnotation[0] == "true") && !flag.Changed {
-			missingFlagNames = append(missingFlagNames, flag.Name)
-		}
-	})
-	if len(missingFlagNames) > 0 {
-		return common.OperationProductionData{},
-			fmt.Errorf(`required flag(s) "%s" not set`, strings.Join(missingFlagNames, `", "`))
+	err = checkRequiredFlags(operFlagSet)
+	if err != nil {
+		return common.OperationProductionData{}, fmt.Errorf("%v", err)
 	}
 
 	data := common.OperationProductionData{
@@ -221,4 +215,21 @@ func (pc *productionCommand) getOperationData(
 		},
 	}
 	return data, nil
+}
+
+func checkRequiredFlags(commandFlags *pflag.FlagSet) error {
+	var missingFlagNames []string
+	commandFlags.VisitAll(func(flag *pflag.Flag) {
+		requiredAnnotation, found := flag.Annotations[RequiredOption]
+		if !found {
+			return
+		}
+		if (requiredAnnotation[0] == "true") && !flag.Changed {
+			missingFlagNames = append(missingFlagNames, flag.Name)
+		}
+	})
+	if len(missingFlagNames) > 0 {
+		return fmt.Errorf(`required flag(s) "%s" not set`, strings.Join(missingFlagNames, `", "`))
+	}
+	return nil
 }
