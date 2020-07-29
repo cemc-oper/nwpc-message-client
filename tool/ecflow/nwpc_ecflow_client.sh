8e94f1466e1011a1a5d456f9064bc0c8acaa76b1
perillaroc <perillaroc@gmail.com>
2020-07-29 09:20:29 +0800

    :truck: move nwpc_ecflow_client.sh into a new directory.

--- tool/ecflow/nwpc_ecflow_client.sh
+++ tool/ecflow/nwpc_ecflow_client.sh
@@ -9,13 +9,17 @@ set -e
 /g1/app/apps/ecflow/4.11.1/bin/ecflow_client "$@"
 
 # call nwpc_message_client
+NWPC_MESSAGE_CLIENT_RABBITMQ_ADDRESS=amqp://guest:guest@10.28.32.114:32771
+
+NWPC_MESSAGE_CLIENT_BROKER_NODE=login_b06
+NWPC_MESSAGE_CLIENT_BROKER_PORT=33384
+
 set +e
-NWPC_MESSAGE_CLIENT_BROKER_NODE=login_a06
 broker_node=$(getent hosts ${NWPC_MESSAGE_CLIENT_BROKER_NODE} | awk '{ print $1 }')
 nwpc_message_client ecflow-client \
     --command-options="$*" \
-    --rabbitmq-server="amqp://guest:guest@10.28.32.114:32771" \
-    --broker-address="${broker_node}:33384" \
+    --rabbitmq-server="${NWPC_MESSAGE_CLIENT_RABBITMQ_ADDRESS}" \
+    --broker-address="${broker_node}:${NWPC_MESSAGE_CLIENT_BROKER_PORT}" \
     --with-broker
 set -e
 
