c1b4e9c6b4c202d4a3470384c143fb1fdac48833
perillaroc <perillaroc@gmail.com>
2020-10-20 15:25:34 +0800

    :sparkles: add script tools for ecFlow job submit.

--- tool/ecflow/nwpc_ecflow_client.sh
+++ tool/ecflow/nwpc_ecflow_client.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-# nwpc_ecflow_client.sh for ecflow in HPC-PI on CMA.
+# nwpc_ecflow_client.sh for ecFlow in HPC-PI on CMA.
 # 1. call ecflow_client
 # 2. call nwpc_message_client to send messages.
+
 set -x
 set -e
 
@@ -9,14 +10,19 @@ set -e
 /g1/app/apps/ecflow/4.11.1/bin/ecflow_client "$@"
 
 # call nwpc_message_client
-NWPC_MESSAGE_CLIENT_RABBITMQ_ADDRESS=amqp://guest:guest@10.28.32.114:32771
 
-NWPC_MESSAGE_CLIENT_BROKER_NODE=login_b06
-NWPC_MESSAGE_CLIENT_BROKER_PORT=33384
+# nwpc_message_client environments, use default values if not set
+export NWPC_MESSAGE_CLIENT_RABBITMQ_ADDRESS=${NWPC_MESSAGE_CLIENT_RABBITMQ_ADDRESS:-"amqp://guest:guest@locahost:9000"}
 
+export NWPC_MESSAGE_CLIENT_BROKER_NODE=${NWPC_MESSAGE_CLIENT_BROKER_NODE:-login_b06}
+export NWPC_MESSAGE_CLIENT_BROKER_PORT=${NWPC_MESSAGE_CLIENT_BROKER_PORT:-33384}
+
+export NWPC_MESSAGE_CLINET_PROGRAM=${NWPC_MESSAGE_CLINET_PROGRAM:-nwpc_message_client@v0.5}
+
+# send message
 set +e
 broker_node=$(getent hosts ${NWPC_MESSAGE_CLIENT_BROKER_NODE} | awk '{ print $1 }')
-nwpc_message_client ecflow-client \
+${NWPC_MESSAGE_CLINET_PROGRAM} ecflow-client \
     --command-options="$*" \
     --rabbitmq-server="${NWPC_MESSAGE_CLIENT_RABBITMQ_ADDRESS}" \
     --broker-address="${broker_node}:${NWPC_MESSAGE_CLIENT_BROKER_PORT}" \
