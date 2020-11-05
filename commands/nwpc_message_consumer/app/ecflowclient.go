2cd0630533709f8a3d4f9516133c9f7f3631a1e5
perillaroc <perillaroc@gmail.com>
2020-11-05 14:18:33 +0800

    :sparkles: add printer consumer. fix #11

--- commands/nwpc_message_consumer/app/ecflowclient.go
+++ commands/nwpc_message_consumer/app/ecflowclient.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"github.com/nwpc-oper/nwpc-message-client/common/consumer"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -20,8 +21,9 @@ type ecflowClientCommand struct {
 
 	consumerType string
 
-	rabbitmqServer    string
-	rabbitmqQueueName string
+	rabbitmqServer       string
+	rabbitmqQueueName    string
+	rabbitmqExchangeName string
 
 	elasticServer string
 
@@ -39,24 +41,35 @@ func (c *ecflowClientCommand) consumerEcflowClient(cmd *cobra.Command, args []st
 
 	source := consumer.RabbitMQSource{
 		Server:   c.rabbitmqServer,
-		Exchange: "nwpc.operation.workflow",
+		Exchange: c.rabbitmqExchangeName,
 		Topics:   []string{"ecflow.command.ecflow_client"},
 		Queue:    c.rabbitmqQueueName,
 	}
 
-	target := consumer.ElasticSearchTarget{
-		Server: c.elasticServer,
+	var currentConsumer consumer.Consumer = nil
+	if c.consumerType == string(printerConsumerType) {
+		currentConsumer = createPrinterConsumer(
+			source,
+			c.workerCount,
+			c.isDebug)
+	} else if c.consumerType == string(elasticsearchConsumerType) {
+		target := consumer.ElasticSearchTarget{
+			Server: c.elasticServer,
+		}
+		currentConsumer = createEcflowClientConsumer(
+			source,
+			target,
+			c.workerCount,
+			c.bulkSize,
+			c.isDebug)
 	}
 
-	ecConsumer := createEcflowClientConsumer(
-		source,
-		target,
-		c.workerCount,
-		c.bulkSize,
-		c.isDebug,
-	)
+	if currentConsumer == nil {
+		log.Fatalf("consumer type is not supported: %s", c.consumerType)
+		return fmt.Errorf("consumer type is not supported: %s", c.consumerType)
+	}
 
-	err := ecConsumer.ConsumeMessages()
+	err := currentConsumer.ConsumeMessages()
 	if err != nil {
 		log.WithFields(log.Fields{
 			"component": "ecflow-client",
@@ -76,15 +89,58 @@ func newEcflowClientCommand() *ecflowClientCommand {
 		RunE:  ec.consumerEcflowClient,
 	}
 
-	ecflowClientCmd.Flags().StringVar(&ec.rabbitmqServer,
-		"rabbitmq-server", "", "rabbitmq server")
-	ecflowClientCmd.Flags().StringVar(&ec.rabbitmqQueueName,
-		"rabbitmq-queue-name", "ecflow-client-queue", "rabbitmq queue name")
-	ecflowClientCmd.Flags().StringVar(&ec.elasticServer,
-		"elasticsearch-server", "", "elasticsearch server")
-	ecflowClientCmd.Flags().IntVar(&ec.workerCount, "worker-count", 2, "worker count")
-	ecflowClientCmd.Flags().IntVar(&ec.bulkSize, "bulk-size", 20, "bulk size")
-	ecflowClientCmd.Flags().BoolVar(&ec.isDebug, "debug", true, "debug mode")
+	ecflowClientCmd.Flags().StringVar(
+		&ec.consumerType,
+		"consumer-type",
+		"elasticsearch",
+		"consumer type, printer or elasticsearch",
+	)
+
+	ecflowClientCmd.Flags().StringVar(
+		&ec.rabbitmqServer,
+		"rabbitmq-server",
+		"",
+		"rabbitmq server",
+	)
+	ecflowClientCmd.Flags().StringVar(
+		&ec.rabbitmqQueueName,
+		"rabbitmq-queue-name",
+		"ecflow-client-queue",
+		"rabbitmq queue name",
+	)
+	ecflowClientCmd.Flags().StringVar(
+		&ec.rabbitmqExchangeName,
+		"rabbitmq-exchange-name",
+		"nwpc.operation.workflow",
+		"rabbitmq exchange name",
+	)
+
+	ecflowClientCmd.Flags().StringVar(
+		&ec.elasticServer,
+		"elasticsearch-server",
+		"",
+		"elasticsearch server",
+	)
+
+	ecflowClientCmd.Flags().IntVar(
+		&ec.workerCount,
+		"worker-count",
+		2,
+		"worker count",
+	)
+	ecflowClientCmd.Flags().IntVar(
+		&ec.bulkSize,
+		"bulk-size",
+		20,
+		"bulk size",
+	)
+
+	ecflowClientCmd.Flags().BoolVar(
+		&ec.isDebug,
+		"debug",
+		true,
+		"debug mode",
+	)
 
 	ecflowClientCmd.MarkFlagRequired("rabbitmq-server")
 	ecflowClientCmd.MarkFlagRequired("rabbitmq-queue-name")
